80740c4dd863a3171c92a62310f07e81b1d67bc9
Mohana Sundaram K <mohanasundaramk06@gmail.com>
2023-06-24 20:47:08 +0530

    Implementation 1.0

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,18 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -20,13 +27,42 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin();
+        admin.setUsername(username);
+        admin.setPassword(password);
+
+        return adminRepository1.save(admin);
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        Admin admin = adminRepository1.findById(adminId).get();
+
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setName(providerName);
+        serviceProvider.setAdmin(admin);
+
+        admin.getServiceProviderList().add(serviceProvider);
+
+        return adminRepository1.save(admin);
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-    }
+
+        List<String> countries= Arrays.asList("AUS","IND","USA","CHI","JPN");
+
+        if(!countries.contains(countryName.toUpperCase())) throw new Exception("Country not found");
+
+        Country country =new Country();
+        country.setCountryName(CountryName.valueOf(countryName));
+        country.setCode(CountryName.valueOf(countryName).toCode());
+
+        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+        country.setServiceProvider(serviceProvider);
+        serviceProvider.getCountryList().add(country);
+
+        return serviceProviderRepository1.save(serviceProvider);
+   }
 }
--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,87 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
+        User user=userRepository2.findById(userId).get();
+        if(user.getConnected()==true)
+            throw new Exception("Already connected");
+        else if(user.getCountry().getCountryName().toString().equalsIgnoreCase(countryName))
+            return user;
+        List<ServiceProvider> serviceProviders=user.getServiceProviderList();
+        ServiceProvider lowestServiceProvider=null;
+        int lowestId=Integer.MAX_VALUE;
+        for(ServiceProvider serviceProvider:serviceProviders){
+            List<Country> countries=serviceProvider.getCountryList();
+            for(Country country:countries){
+                if(countryName.equalsIgnoreCase(country.getCountryName().toString())){
+                    if(lowestServiceProvider==null || lowestId > serviceProvider.getId()){
+                        lowestId=serviceProvider.getId();
+                        lowestServiceProvider=serviceProvider;
+                    }
+                }
+            }
+        }
 
+        if(lowestServiceProvider==null)
+            throw new Exception("Unable to connect");
+        //make connection
+        String maskedIpAd=CountryName.valueOf(countryName.toUpperCase()).toCode()+"."+lowestServiceProvider.getId()+"."+userId;
+        user.setMaskedIp(maskedIpAd);
+        user.setConnected(true);
+        Connection connection=new Connection();
+        connection.setServiceProvider(lowestServiceProvider);
+        connection.setUser(user);
+        user.getConnectionList().add(connection);
+        lowestServiceProvider.getConnectionList().add(connection);
+        userRepository2.save(user);
+        serviceProviderRepository2.save(lowestServiceProvider);
+        return user;
     }
     @Override
     public User disconnect(int userId) throws Exception {
 
+        User user=userRepository2.findById(userId).get();
+        if(user.getConnected()==false)
+            throw new Exception("Already disconnected");
+        user.setConnected(false);
+        user.setMaskedIp(null);
+
+        return userRepository2.save(user);
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
+        User receiver=userRepository2.findById(receiverId).get();
+        CountryName receiverCountryName=null;
 
+        if(receiver.getConnected()){
+            String maskedCode= receiver.getMaskedIp().substring(0,3);
+            switch (maskedCode) {
+                case "001":
+                    receiverCountryName = CountryName.IND;
+                    break;
+                case "002":
+                    receiverCountryName = CountryName.USA;
+                    break;
+                case "003":
+                    receiverCountryName = CountryName.AUS;
+                    break;
+                case "004":
+                    receiverCountryName = CountryName.CHI;
+                    break;
+                case "005":
+                    receiverCountryName = CountryName.JPN;
+                    break;
+            }
+        }
+        else{
+            receiverCountryName=receiver.getCountry().getCountryName();
+        }
+        User user=null;
+        try{
+            user=connect(senderId,receiverCountryName.toString());
+        }
+        catch(Exception e){
+            throw new Exception("Cannot establish communication");
+        }
+        return user;
     }
 }
